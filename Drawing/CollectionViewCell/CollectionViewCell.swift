bae57c1347661c070200f7b06c4f97b39d4d93a6
kchinna <kchinnababu@zeomega.com>
2018-02-08 10:42:46 +0530

    test

---
+++ Drawing/CollectionViewCell/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  Drawing
+//
+//  Created by Chinnababu on 2/8/18.
+//  Copyright © 2018 Chinnababu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var descrptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
