75d56e68a08fb6f2356cd3ad110c66b8b1dbf5f5
chinna <k.chinnababu@zinier.com>
2020-01-20 15:24:55 +0530

    Initial Commit

---
+++ DataGridCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataGridCollectionView
+//
+//  Created by chinnababu on 20/01/20.
+//  Copyright © 2020 chinnababu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
