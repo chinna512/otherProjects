36a2ada6610010b996008524254002900ac0175b
chinn <K.CHINNababu@zinier.com>
2020-06-07 12:00:52 +0530

    Initial Commit

---
+++ MahiCreations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MahiCreations
+//
+//  Created by k.chinnababu on 07/06/20.
+//  Copyright © 2020 Mahi Info services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
